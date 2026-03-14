93c17675d50c9dee5cc0d4af5f258adced0ad07a
deepsmc21 <deepthi.mc@gmail,com>
2026-03-14 12:13:23 +0100

    Add HTML pages and Express routes for index, theme, login and downloads

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
+const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, "public")))
-
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/html/index.html"))
-})
+  res.sendFile(path.join(__dirname, "public", "html", "index.html"));
+});
 
 app.get("/thema", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/html/thema.html"))
-})
+  res.sendFile(path.join(__dirname, "public", "html", "thema.html"));
+});
 
 app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/html/login.html"))
-})
+  res.sendFile(path.join(__dirname, "public", "html", "login.html"));
+});
 
 app.get("/downloads", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/html/downloads.html"))
-})
+  res.sendFile(path.join(__dirname, "public", "html", "downloads.html"));
+});
 
-app.listen(3000, () => {
-    console.log("Server läuft auf http://localhost:3000")
-})
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  console.log("Login empfangen:");
+  console.log("Benutzername:", username);
+  console.log("Passwort:", password);
+
+  res.send("Login-Anfrage wurde empfangen.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf http://localhost:${PORT}`);
+});

95192cedbbbfb540eedce740c9a5774f897ec32b
deepsmc21 <deepthi.mc@gmail,com>
2026-03-14 13:13:16 +0100

    Implement basic login check with MySQL user lookup

--- server.js
+++ server.js
@@ -37,3 +37,28 @@ app.post("/login", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server läuft auf http://localhost:${PORT}`);
 });
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const sql = "SELECT * FROM users WHERE username = ?";
+
+  db.query(sql, [username], (err, results) => {
+    if (err) {
+      console.error("Fehler bei der Datenbankabfrage:", err);
+      return res.status(500).send("Datenbankfehler");
+    }
+
+    if (results.length === 0) {
+      return res.send("Benutzer nicht gefunden.");
+    }
+
+    const user = results[0];
+
+    if (user.password === password) {
+      return res.send(`Login erfolgreich. Willkommen ${user.username}!`);
+    } else {
+      return res.send("Falsches Passwort.");
+    }
+  });
+});
